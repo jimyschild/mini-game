25e0d226c9a1e99aa7b2da3f46f59e9a082c9ee5
Jimy Oliveira Schild <jimyoliveiraschild@gmail.com>
2023-04-27 19:49:15 -0300

    ajustando estilos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gameBoard = document.querySelector('.game-board')
+const campo = document.getElementById('campo')
 const boneco = document.querySelector('.boneco')
 const obstaculo = document.querySelector('.obstaculo')
 const começarJogo = document.getElementById('começa-jogo')
@@ -12,18 +13,21 @@ const jump = () => {
         boneco.classList.remove('jump')
     },600)
 }
-document.addEventListener('click', jump)
+gameBoard.addEventListener('click', jump)
 //--------movimento osbataculo
 const loop = setInterval(() => {
 
     const obstaculoPosition = obstaculo.offsetLeft;
     const bonecoPosition = +window.getComputedStyle(boneco).bottom.replace('px', '')
-
+    
     if(obstaculoPosition <= 75 && obstaculoPosition > -20 && bonecoPosition < 50){
         obstaculo.style.animation = 'none'
         obstaculo.style.left = `${obstaculoPosition}px`
         boneco.style.animation = 'none'
         boneco.style.bottom = `${bonecoPosition}px`
+        boneco.style.border = 'solid 2px #000706'
+        boneco.style.boxShadow = '0px 0px 10px var(--color1), 0px 0px 25px var(--color1) inset'
+
     }
    
 }, 10)
@@ -45,13 +49,14 @@ resetJogo.addEventListener('click', reset)
 const divDistancia =() => {
     
     let distancia = document.createElement('div')
-    gameBoard.appendChild(distancia)
+    campo.appendChild(distancia)
     distancia.style.border = '2px solid green'
-    distancia.style.width = '200px'
-    distancia.style.height = '20px'
-    distancia.style.marginTop = '100px'
-    distancia.style.marginLeft = '50px'
-    distancia.innerText = `Distância percorrid`
+    distancia.style.width = '250px'
+    distancia.style.height = '30px'
+    distancia.style.marginTop = '-536px'
+    distancia.style.marginLeft = '100px'
+    distancia.style.fontSize = '21px'
+    distancia.innerText = `Distância percorrida:`
     return distancia
 }
 divDistancia()
