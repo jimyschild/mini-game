de7ac39d076a95a78499050957d43b323c128fbf
Jimy Oliveira Schild <jimyoliveiraschild@gmail.com>
2023-05-01 18:25:20 -0300

    adicionanco distância percorrida

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ const começarJogo = document.getElementById('começa-jogo')
 const resetJogo = document.getElementById('reseta-jogo')
 const cloud = document.querySelector('.cloud')
 const cloud2 = document.querySelector('.cloud2')
+const distancia = document.querySelector('.distancia')
 
 //-------pulo
+
 const jump = () => {
     boneco.classList.add('jump')
+    
 
     setTimeout(() =>{
         boneco.classList.remove('jump')
     },600)
 }
 gameBoard.addEventListener('click', jump)
-//--------movimento osbataculo
-const loop = setInterval(() => {
 
+//--------movimento osbataculo
+
+const loop = setInterval(() => {
+    
     const obstaculoPosition = obstaculo.offsetLeft;
     const cloudPosition = cloud.offsetLeft;
     const cloud2Position = cloud2.offsetLeft;
@@ -35,38 +40,34 @@ const loop = setInterval(() => {
         boneco.style.bottom = `${bonecoPosition}px`
         boneco.style.border = 'solid 2px #000706'
         boneco.style.boxShadow = '0px 0px 10px var(--color1), 0px 0px 25px var(--color1) inset'
-
     }
-   
 }, 10)
 
-
 //--------Começar o jogo
+
 const start = () => {
     obstaculo.classList.add('começar')
     cloud.classList.add('clouds')
-    cloud2.classList.add('clouds2')
+    cloud2.classList.add('clouds2') 
+
+    let count = 0;
+
+    setInterval(function() {
+    count++;
+    distancia.innerText = `Distância percorrida: ${count}`
+    }, 2000);
 }
 começarJogo.addEventListener('click', start)
 
 //--------reseta o jogo/recarrega a pagina
+
 const reset = () => {
     location.reload()
 }
 resetJogo.addEventListener('click', reset)
 
 //-------- distancia percorrida
-const divDistancia =() => {
-    
-    let distancia = document.createElement('div')
-    campo.appendChild(distancia)
-    // distancia.style.border = '2px solid green'
-    distancia.style.width = '250px'
-    distancia.style.height = '30px'
-    distancia.style.marginTop = '-536px'
-    distancia.style.marginLeft = '100px'
-    distancia.style.fontSize = '21px'
-    distancia.innerText = `Distância percorrida:`
-    return distancia
-}
-divDistancia()
+
+
+
+
